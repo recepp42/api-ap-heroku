599c8c1646c47251e2e191d89fc8dead5b245269
recepp42 <57023556+recepp42@users.noreply.github.com>
2022-02-16 16:39:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(cors());
 app.options('*', cors({{
         credentials: true,
-        origin: 'file:///D:/softwaretesting/sec/index.html'}));
+        origin: 'https://zen-clarke-0d9d12.netlify.app/'}));
 
 app.get('/', (req, res) => res.send('‘I modified this API'));
 
