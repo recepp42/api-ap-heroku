a48c4a36fd8b5a93572068c23876a4046a900288
recepp42 <57023556+recepp42@users.noreply.github.com>
2022-02-16 14:43:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://brave-boyd-6c3c10.netlify.app'
+        origin: 'https://zen-clarke-0d9d12.netlify.app/'
     })
 );
 app.options('*', cors());
